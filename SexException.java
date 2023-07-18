39908543ea1916af1e8570e93f891adf1758a15a
Екатерина Демина <111079583+Ekaterina2709@users.noreply.github.com>
2023-07-18 13:39:49 +0300

    Update SexException.java

--- SexException.java
+++ SexException.java
@@ -5,7 +5,7 @@ public class SexException extends Exception {
     }
 
     public void sexException(String i) {
-        System.out.println("Исключение: Неверные данные в графе "Пол"");
+        System.out.println("Исключение: Неверные данные в графе Пол");
         System.out.printf("Некорректные данные: %s", i);
         System.out.println();
     }
