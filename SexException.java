d7803f6001bccb698cfcf488558cf338dbb11dbf
Екатерина Демина <111079583+Ekaterina2709@users.noreply.github.com>
2023-07-18 13:07:46 +0300

    Create SexException.java
    SexException

---
+++ SexException.java
@@ -0,0 +1,12 @@
+package javaErrorException.HomeWork;
+
+public class SexException extends Exception {
+    public SexException() {
+    }
+
+    public void sexException(String i) {
+        System.out.println("Исключение: Неверные данные в графе "Пол"");
+        System.out.printf("Некорректные данные: %s", i);
+        System.out.println();
+    }
+}
